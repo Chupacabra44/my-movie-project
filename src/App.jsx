1f3d6e243f08a6451286e1b217b98a3c7fa21f1c
Sanja Daic <sanja.daic86@gmail.com>
2026-01-30 15:03:29 +0100

    creating bg image and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <header className="flex justify-between items-center p-4 bg-[#58225a] text-white">
+        <span>Logo</span>
+        <nav className="space-x-4">
+          <ul className="flex space-x-4 gap-2">
+            <li>Home</li>
+            <li>Info</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+      <div className="overlay"></div>
+    </>
+  );
 };
 
 export default App;
