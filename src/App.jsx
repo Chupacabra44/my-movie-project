6953289c313b35b8e2ed8d31ccce84f9ccffe954
Sanja Daic <sanja.daic86@gmail.com>
2026-02-02 08:24:44 +0100

    adding some styling

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,20 @@ const App = () => {
         </nav>
       </header>
       <div className="overlay"></div>
+      <main className="hero-section flex flex-col items-center justify-center text-center p-8">
+        <img
+          className="w-full max-w-lg h-auto object-contain mx-auto drop-shadow-md"
+          src="/images/hero-img.png"
+          alt="Hero image"
+        />
+      </main>
+      <h1 className="text-white text-6xl font-bold text-center mt-8">
+        Find{" "}
+        <span className="bg-linear-to-r from-[#ccc5ff] to-[#972eff] bg-clip-text text-transparent">
+          Movies
+        </span>{" "}
+        You'll Enjoy <br /> Without the Hassle
+      </h1>
     </>
   );
 };
