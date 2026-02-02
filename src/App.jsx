7186681e310b4e36734f3a99232492090f61d322
Sanja Daic <sanja.daic86@gmail.com>
2026-02-02 09:34:46 +0100

    adding more style for card component

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,26 @@ const App = () => {
         </span>{" "}
         You'll Enjoy <br /> Without the Hassle
       </h1>
+      <section className="flex flex-col items-start mt-12 px-8 text-white">
+        <h2 className="text-4xl z-10 ps-14 mt-14 relative">All Movies</h2>
+        <div className="flex flex-wrap flex-col gap-8 justify-center mb-16 bg-[#241a1b]  min-w-100 m-10 z-20 rounded-2xl">
+          <img
+            className="z-20 max-w-80 h-auto object-contain m-10 rounded-2xl"
+            src="/images/No-Poster.png"
+            alt="Movie poster"
+          />
+          <div className="flex flex-col gap-4 z-20 p-5">
+            <h3>Movie name</h3>
+            <div className="flex gap-4 items-center justify-items-start z-20">
+              <div>
+                <img src="/images/star-1.png" alt="" />
+              </div>
+              <span>8.5</span>
+              <span>2023</span>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
