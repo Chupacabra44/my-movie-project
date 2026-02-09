a4875226746ed8e4fa67174e452259d53a464cd5
Sanja Daic <sanja.daic86@gmail.com>
2026-02-09 08:27:22 +0100

    adding button funcionality and polishing useEffect

--- src/components/RelatedMovies.jsx
+++ src/components/RelatedMovies.jsx
@@ -34,9 +34,15 @@ const RelatedMovies = () => {
 
   useEffect(() => {
     const fetchRelatedMovies = async () => {
+      if (!genreId.length) {
+        setRelatedMovie([]);
+        setLoading(false);
+        return;
+      }
+
       try {
-        if (!genreId.length) return;
         setLoading(true);
+
         const relatedRes = await fetch(
           `https://api.themoviedb.org/3/discover/movie?with_genres=${genreId.join(",")}&sort_by=${sortBy}&page=${currentPage}`,
           API_OPTIONS,
@@ -48,13 +54,14 @@ const RelatedMovies = () => {
 
         const relatedMovies = await relatedRes.json();
         setRelatedMovie(relatedMovies.results);
-        setLoading(false);
       } catch (error) {
         console.log(error);
         setError("Failed to load movies!");
+      } finally {
         setLoading(false);
       }
     };
+
     fetchRelatedMovies();
   }, [genreId, sortBy, currentPage]);
 
@@ -78,13 +85,28 @@ const RelatedMovies = () => {
         </p>
       ) : (
         <>
-          <div className="text-white flex flex-wrap justify-end m-10 z-20 rounded-2xl">
-            <button
-              onClick={() => setCurrentPage((prev) => prev + 1)}
-              className="font-bold border-solid border-2 px-4 py-2 rounded-2xl hover:bg-amber-600 hover:text-gray-800 border-amber-300 cursor-pointer"
-            >
-              Page {currentPage} →
-            </button>
+          <div className="relative text-white flex flex-wrap justify-end m-10 z-20 rounded-2xl">
+            {currentPage === 1 ? (
+              ""
+            ) : (
+              <button
+                onClick={() => setCurrentPage((prev) => prev - 1)}
+                className="absolute left-0 font-bold border-solid border-2 px-4 py-2 rounded-2xl hover:bg-amber-600 hover:text-gray-800 border-amber-300 cursor-pointer"
+              >
+                ← Page {currentPage - 1}
+              </button>
+            )}
+
+            {!loading && relatedMovie.length === 0 ? (
+              <p>No more related movies!</p>
+            ) : (
+              <button
+                onClick={() => setCurrentPage((prev) => prev + 1)}
+                className="font-bold border-solid border-2 px-4 py-2 rounded-2xl hover:bg-amber-600 hover:text-gray-800 border-amber-300 cursor-pointer"
+              >
+                Page {currentPage} →
+              </button>
+            )}
           </div>
           <div className="text-white flex flex-wrap flex-row gap-8 justify-center mb-16 min-w-100 m-10 z-20 rounded-2xl">
             {relatedMovie.map((movie) => (
